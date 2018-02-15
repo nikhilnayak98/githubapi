63c9c5a11dad4e250a00c09b5b3b5a6b28386fff
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-15 21:25:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+import json
+import base64
+import requests
+import string
+import random
+import schedule
+import time
+
+def job():
+    token = "b7885278fa80397a101f61daae366584aed3e3a4"
+    filename = "README.md"
+    repo = "nikhilnayak98/gitcommitter"
+    branch = "master"
+    push_to_github(filename, repo, branch, token)
+    print("I'm working...")
+
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+def generate_string(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def push_to_github(filename, repo, branch, token):
+    url = "https://api.github.com/repos/" + repo + "/contents/" + filename
+
+    filecontents = generate_string()
+
+    with open(filename, "w") as f:
+        f.write(filecontents)
+
+    base64content = base64.b64encode(open(filename,"rb").read())
+
+    data = requests.get(url+'?ref='+branch, headers = {"Authorization": "token "+token}).json()
+    print(data)
+    sha = data['sha']
+
+    if base64content.decode('utf-8')+"\n" != data['content']:
+        message = json.dumps({"message": generate_string(3, filecontents),
+                            "branch": branch,
+                            "content": base64content.decode("utf-8") ,
+                            "sha": sha
+                            })
+
+        resp=requests.put(url, data = message, headers = {"Content-Type": "application/json", "Authorization": "token "+token})
+
+        print(resp)
+    else:
+        print("nothing to update")
+    
